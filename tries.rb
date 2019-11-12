2ce06230557d6f00d5f33ede1761fb574b4fd6c0
Rustam Ibragimov <iamdaiver@gmail.com>
2019-11-13 01:09:17 +0300

    [magic] wat :\

--- tries.rb
+++ tries.rb
@@ -22,3 +22,23 @@ end
 with_boost def omfg
   puts "NIHERA_SEBE"
 end
+
+module Diablo
+  class << self
+
+  end
+end
+
+__END__
+
+RubyVM::InstructionSequence.compile_option = {
+  tailcall_optimization: true,
+  trace_instruction: false,
+}
+
+RubyVM::InstructionSequence.new(<<~EOS).eval
+  def fibo(req = 1, start = 1, l = 0, r = 1)
+    return r if start == req
+    fibo(req, start + 1, r, l + r)
+  end
+EOS
